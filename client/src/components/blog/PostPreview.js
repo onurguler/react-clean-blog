faf187605b48da1356eb3d2fa6a9799abf0c6506
Onur Güler <guleronurosman@gmail.com>
2019-07-30 16:59:33 +0300

    format date and add Link

--- client/src/components/blog/PostPreview.js
+++ client/src/components/blog/PostPreview.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const PostPreview = ({
-  blog: { name, title, comSentence, comImage, date }
-}) => {
-  return (
-    <div className="row">
-      <div className="col-lg-4 mx-auto my-auto">
-        {comImage && (
+  blog: { _id, name, title, comSentence, comImage, date }
+}) => (
+  <div className="row">
+    <div className="col-lg-4 mx-auto my-auto">
+      {comImage && (
+        <Link to={`/blogs/${_id}`}>
           <div className="img text-center">
             <img
               className="img-thumbnail rounded"
@@ -15,24 +17,26 @@ const PostPreview = ({
               alt="blog image"
             />
           </div>
-        )}
-      </div>
-
-      <div className="col-lg-8 col-md-10 mx-auto my-auto">
-        <div className="post-preview">
-          <a href="post.html">
-            <h2 className="post-title">{title}</h2>
-            {comSentence && <h3 className="post-subtitle">{comSentence}</h3>}
-          </a>
-          <p className="post-meta">
-            Posted by <a href="#">{name}</a> on {date}
-          </p>
-        </div>
-        <hr />
-      </div>
+        </Link>
+      )}
     </div>
-  );
-};
+
+    <div className="col-lg-8 col-md-10 mx-auto my-auto">
+      <div className="post-preview">
+        <Link to={`/blogs/${_id}`}>
+          <h2 className="post-title">{title}</h2>
+          {comSentence && <h3 className="post-subtitle">{comSentence}</h3>}
+        </Link>
+        <p className="post-meta">
+          Posted by <a href="#">{name}</a> on{' '}
+          {moment(date).format('MMMM DD, YYYY')} at{' '}
+          {moment(date).format('h:mm:ss a')}
+        </p>
+      </div>
+      <hr />
+    </div>
+  </div>
+);
 
 PostPreview.propTypes = {
   blog: PropTypes.object.isRequired
